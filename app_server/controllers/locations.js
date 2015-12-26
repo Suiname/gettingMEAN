494eedf453b17a2a70c9eb1011cb7393d2846f48
Jason Tham <jtham@sbcglobal.net>
2015-12-25 18:13:30 -0600

    added new routes, added controllers

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+//Get home page
+module.exports.homelist = function(req, res) {
+  res.render('index', { title: 'Home'});
+};
+
+//get Location info page
+module.exports.locationInfo = function(req, res) {
+  res.render('index', { title: 'Location info'});
+};
+
+//get Add review page
+module.exports.addReview = function(req, res){
+  res.render('index', { title: 'Add review'});
+};
