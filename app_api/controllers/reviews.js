81064a44f53cf2113abcbb070b99736ca7bd9fdf
Jason Tham <jtham@sbcglobal.net>
2015-12-31 15:07:49 -0600

    added api but with errors

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,8 +1,11 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model("Location");
+
 var sendJsonResponse = function(res, status, content){
   res.status(status);
   res.json(content);
 };
 
-module.exports.locationsCreate = function (req, res){
+module.exports.reviewsCreate = function (req, res){
   sendJsonResponse(res, 200, {"status" : "success"});
 };
