9518ea59186c69faa522d3027bae4cbe18ec9e23
Jason Tham <jtham@sbcglobal.net>
2015-12-31 17:53:47 -0600

    finished chapter 6

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,16 +3,12 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-var homepageController = function (req, res){
-  res.render('index', { title: 'Express' });
-};
-
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
-// Other pages
+/* Other pages */
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
