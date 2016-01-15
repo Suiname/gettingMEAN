e85a0c0215d98c1aa6f10dd0faed9a4345e4e3f3
Jason Tham <jtham@sbcglobal.net>
2016-01-14 20:45:10 -0600

    added jquery validation of form

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -221,16 +221,20 @@ module.exports.doAddReview = function(req, res){
     method: "POST",
     json: postdata
   };
-  request(
-    requestOptions,
-    function(err, response, body) {
-      if (response.statusCode === 201){
-        res.redirect('/location/' + locationid);
-      } else if (response.statusCode === 400 && body.name && body.name === "ValidationError") {
-        res.redirect('/location/' + locationid + "/reviews/new?err=val");
-      } else {
-        _showError(req, res, response.statusCode);
+  if (!postdata.author || !postdata.raing || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+  } else {
+    request(
+      requestOptions,
+      function(err, response, body) {
+        if (response.statusCode === 201){
+          res.redirect('/location/' + locationid);
+        } else if (response.statusCode === 400 && body.name && body.name === "ValidationError") {
+          res.redirect('/location/' + locationid + "/reviews/new?err=val");
+        } else {
+          _showError(req, res, response.statusCode);
+        }
       }
-    }
-  );
+    );
+  }
 };
