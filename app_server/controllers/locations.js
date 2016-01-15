b6f70185130f646025eecac8cce488266b151d0c
Jason Tham <jtham@sbcglobal.net>
2016-01-14 22:38:32 -0600

    added angular controller and put data inside scope

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -33,16 +33,7 @@ if (process.env.NODE_ENV === 'production'){
 //         distance: '250m'
 //     }]
 // });
-var renderHomepage = function(req, res, responseBody){
-  var message;
-  if (!(responseBody instanceof Array)) {
-    message = "API lookup error";
-    responseBody = [];
-  } else {
-    if (!responseBody.length){
-      message = "No places found nearby";
-    }
-  }
+var renderHomepage = function(req, res){
   res.render('locations-list', {
       title: 'Loc8r - find a place to work with wifi',
       pageHeader: {
@@ -68,31 +59,7 @@ var _formatDistance = function(distance){
 };
 
 module.exports.homelist = function(req, res) {
-  var requestOptions, path;
-  path = '/api/locations';
-  requestOptions = {
-    url : apiOptions.server + path,
-    method: "GET",
-    json: {},
-    qs: {
-      lng: -0.7992599,
-      lat: 51.378091,
-      maxDistance: 20
-    }
-  };
-  request(
-    requestOptions,
-    function(err, response, body){
-      var i, data;
-      data = body;
-      if (response.statusCode === 200 && data.length){
-        for (i=0; i<data.length; i++){
-          data[i].distance = _formatDistance(data[i].distance);
-        }
-      }
-      renderHomepage(req, res, data);
-    }
-  );
+  renderHomepage(req, res);
 };
 
 /* GET 'Location info' page */
--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,1 +1,24 @@
 angular.module('loc8rApp', []);
+
+var locationsListCtrl = function($scope){
+  $scope.data = {
+    locations: [{
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 IPS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', "Premium wifi"],
+      distance: '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Costy',
+      addess: '125 High Street, Reading, RG6 IPS',
+      rating: 5,
+      facilities: ['Hot Drinks', "Food", "Alcoholic Drinks"],
+      distance: "0.7865456",
+      _id: '5370a35f2536f6785f8dfb6a'
+    }]};
+}
+
+angular
+  .module('loc8rApp')
+  .controller('locationsListCtrl', locationsListCtrl);
