fef4b3ecfdeba284b5100c99c9b7bae0b9d71035
Jason Tham <jtham@sbcglobal.net>
2016-01-17 18:30:08 -0600

    added location data service in angular

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,22 +1,7 @@
 angular.module('loc8rApp', []);
 
-var locationsListCtrl = function($scope){
-  $scope.data = {
-    locations: [{
-      name: 'Burger Queen',
-      address: '125 High Street, Reading, RG6 IPS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', "Premium wifi"],
-      distance: '0.296456',
-      _id: '5370a35f2536f6785f8dfb6a'
-    },{
-      name: 'Costy',
-      addess: '125 High Street, Reading, RG6 IPS',
-      rating: 5,
-      facilities: ['Hot Drinks', "Food", "Alcoholic Drinks"],
-      distance: "0.7865456",
-      _id: '5370a35f2536f6785f8dfb6a'
-    }]};
+var locationsListCtrl = function($scope, loc8rData){
+  $scope.data = { locations: loc8rData};
 };
 
 var _isNumeric = function(n) {
@@ -50,8 +35,20 @@ var ratingStars = function(){
   };
 };
 
+var loc8rData = function(){
+  return [{
+    name: 'Burger Queen',
+    address: '125 High Street, Reading, RG6 IPS',
+    rating: 3,
+    facilities: ['Hot drinks', 'Food', "Premium wifi"],
+    distance: '0.296456',
+    _id: '5370a35f2536f6785f8dfb6a'
+  }];
+}
+
 angular
   .module('loc8rApp')
   .controller('locationsListCtrl', locationsListCtrl)
   .filter('formatDistance', formatDistance)
-  .directive('ratingStars', ratingStars);
+  .directive('ratingStars', ratingStars)
+  .service('loc8rData', loc8rData);
