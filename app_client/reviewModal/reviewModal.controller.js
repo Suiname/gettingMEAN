e2784e1ab8e0d202900b2d9ef36cdccd883cd25a
Jason Tham <jtham@sbcglobal.net>
2016-02-05 21:49:36 -0600

    fixed a bug with bootstrap

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -1,4 +1,5 @@
-(function(){
+(function () {
+
   angular
     .module('loc8rApp')
     .controller('reviewModalCtrl', reviewModalCtrl);
@@ -8,9 +9,9 @@
     var vm = this;
     vm.locationData = locationData;
 
-    vm.onSubmit = function(){
+    vm.onSubmit = function () {
       vm.formError = "";
-      if(!vmformData.name || !vm.formData.rating || !vm.formData.reviewText){
+      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
         vm.formError = "All fields required, please try again";
         return false;
       } else {
@@ -18,28 +19,30 @@
       }
     };
 
-    vm.doAddReview = function(locationid, formData){
+    vm.doAddReview = function (locationid, formData) {
       loc8rData.addReviewById(locationid, {
-        author: formData.name,
-        rating: formData.rating,
+        author : formData.name,
+        rating : formData.rating,
         reviewText : formData.reviewText
       })
-        .success(function(data){
+        .success(function (data) {
           vm.modal.close(data);
         })
-        .error(function(data){
-          vm.formError = "Your review has not been saved, try again";
+        .error(function (data) {
+          vm.formError = "Your review has not been saved, please try again";
         });
       return false;
     };
 
     vm.modal = {
-      close: function(result){
+      close : function (result) {
         $modalInstance.close(result);
       },
-      cancel: function() {
+      cancel : function () {
         $modalInstance.dismiss('cancel');
       }
     };
+
   }
+
 })();
