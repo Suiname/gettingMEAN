2514dc2af5c4423453b59e0036f788915d53ee4f
Jason Tham <jtham@sbcglobal.net>
2016-02-05 21:15:41 -0600

    updated review modal

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -3,9 +3,23 @@
     .module('loc8rApp')
     .controller('reviewModalCtrl', reviewModalCtrl);
 
-  reviewModalCtrl.$inject = ['$modalInstance'];
-  function reviewModalCtrl ($modalInstance) {
+  var modalInstance = $modal.open({
+    templateUrl: '/reviewModal/reviewModal.view.html',
+    controller: 'reviewModalCtrl as vm',
+    resolve: {
+      locationData: function(){
+        return{
+          locationid: vm.locationid,
+          locationName: vm.data.location.name
+        };
+      }
+    }
+  });
+
+  reviewModalCtrl.$inject = ['$modalInstance', 'locationData'];
+  function reviewModalCtrl ($modalInstance, locationData) {
     var vm = this;
+    vm.locationData = locationData;
 
     vm.modal = {
       cancel: function() {
